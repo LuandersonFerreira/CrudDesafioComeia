6064e04021a312a8928e45baeaf03953eb697bc3
Luanderson Ferreira <luandersonferreira123@gmail.com>
2020-11-10 17:55:39 -0300

    Commit Final, projeto pronto

---
+++ DesafioCrudMvcComeia/Models/DbModels.cs
@@ -0,0 +1,23 @@
+﻿namespace DesafioCrudMvcComeia.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+
+    public partial class DbModels : DbContext
+    {
+        public DbModels()
+            : base("name=DbModels")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+           // throw new UnintentionalCodeFirstException();
+        }
+
+        public virtual DbSet<Empresas> Empresas { get; set; }
+    }
+
+}
